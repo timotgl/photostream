3cf84f7256735d2d50db40d30d1bfbc071be35d5
Timo Taglieber <github@timotaglieber.de>
2019-09-16 11:28:12 +0200

    Remove some "create react app" defaults

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../../logo.svg';
 import './App.css';
 
 interface Props {
@@ -10,22 +9,16 @@ interface Props {
 }
 
 const App: React.FC<Props> = ({ currentPhotoUrl, showNextPhoto, showPreviousPhoto, fetchAlbum }) => {
-  const url = `https://timotaglieber.de/photos/photos/3840/${currentPhotoUrl || ''}`;
+  const url = `https://timotaglieber.de/photos/photos/1920/${currentPhotoUrl || ''}`;
   const style = currentPhotoUrl ? { backgroundImage: `url('${url}')` } : {};
   return (
     <div className="App" style={style}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <button onClick={fetchAlbum}>fetch album</button>
-        <button onClick={showNextPhoto}>show next photo</button>
-        <button onClick={showPreviousPhoto}>show previous photo</button>
-        <p>
-          current photo url: {currentPhotoUrl}
-        </p>
-      </header>
+      <button onClick={fetchAlbum}>fetch album</button>
+      <button onClick={showNextPhoto}>show next photo</button>
+      <button onClick={showPreviousPhoto}>show previous photo</button>
+      <p>
+        current photo url: {currentPhotoUrl}
+      </p>
     </div>
   );
 };
