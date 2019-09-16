5155f2d3d63ca9eaa9b701fabc9fc2ce1abe30e8
Timo Taglieber <github@timotaglieber.de>
2019-09-16 13:56:28 +0200

    Convert App to class component so we can fetchAlbum once after mounting

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-import './App.css';
 import NavigationHelp from '../NavigationHelp/NavigationHelp';
 import Counter from '../Counter/Counter';
 import PhotoDetails from '../PhotoDetails/PhotoDetails';
+
+import './App.css';
 
 interface Props {
   currentPhotoUrl?: string;
@@ -12,20 +13,27 @@ interface Props {
   fetchAlbum: () => void;
 }
 
-const App: React.FC<Props> = ({ currentPhotoUrl, showNextPhoto, showPreviousPhoto, fetchAlbum }) => {
-  const url = `https://timotaglieber.de/photos/photos/1920/${currentPhotoUrl || ''}`;
-  const style = currentPhotoUrl ? { backgroundImage: `url('${url}')` } : {};
-  return (
-    <div className="App" style={style}>
-      <NavigationHelp />
-      <Counter counter={1} />
-      <PhotoDetails file="test.jpg" title="testTitle" location="testLocation" date="testDate" />
-      <button onClick={fetchAlbum}>fetch album</button>
-      <button onClick={showNextPhoto}>show next photo</button>
-      <button onClick={showPreviousPhoto}>show previous photo</button>
-      <p>current photo url: {currentPhotoUrl}</p>
-    </div>
-  );
-};
+class App extends React.PureComponent<Props> {
+  componentDidMount(): void {
+    this.props.fetchAlbum();
+  }
+
+  render(): React.ReactNode {
+    const { currentPhotoUrl, showNextPhoto, showPreviousPhoto, fetchAlbum } = this.props;
+    const url = `https://timotaglieber.de/photos/photos/1920/${currentPhotoUrl || ''}`;
+    const style = currentPhotoUrl ? { backgroundImage: `url('${url}')` } : {};
+    return (
+      <div className="App" style={style}>
+        <NavigationHelp />
+        <Counter counter={1} />
+        <PhotoDetails file="test.jpg" title="testTitle" location="testLocation" date="testDate" />
+        <button onClick={fetchAlbum}>fetch album</button>
+        <button onClick={showNextPhoto}>show next photo</button>
+        <button onClick={showPreviousPhoto}>show previous photo</button>
+        <p>current photo url: {currentPhotoUrl}</p>
+      </div>
+    );
+  }
+}
 
 export default App;
