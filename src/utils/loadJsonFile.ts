a6a2e346d8af6a22459dbe776a0669650b8c870c
Timo Taglieber <github@timotaglieber.de>
2019-09-16 11:28:30 +0200

    Improve readability

--- src/utils/loadJsonFile.ts
+++ src/utils/loadJsonFile.ts
@@ -1,12 +1,15 @@
 const loadJsonFile = (file: string): Promise<object> => new Promise((resolve) => {
     const rawFile: XMLHttpRequest = new XMLHttpRequest();
+
     rawFile.overrideMimeType('application/json');
     rawFile.open('GET', file, true);
     rawFile.onreadystatechange = () => {
-      if (rawFile.readyState === 4 && (rawFile.status === 200 || rawFile.status === 0)) {
+      const { readyState, status } = rawFile;
+      if (readyState === 4 && (status === 200 || status === 0)) {
         resolve(JSON.parse(rawFile.responseText));
       }
     };
+
     rawFile.send(null);
 });
 
