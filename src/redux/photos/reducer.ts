6743ec91f6f2a61acb6c521d6eceac2fdf6812f2
Timo Taglieber <github@timotaglieber.de>
2019-09-20 13:49:10 +0200

    Use switch statement instead of several ifs

--- src/redux/photos/reducer.ts
+++ src/redux/photos/reducer.ts
@@ -22,32 +22,32 @@ const findPhotoIndexForFile = (items: Array<PhotoItem>, file: string, currentInd
 
 const photosReducer = (state: State = initialState, action: AnyAction): State => {
   let newIndex: number;
-  if (action.type === actionTypes.PHOTOS.SHOW_NEXT) {
-    newIndex = state.currentIndex === initialState.currentIndex ? state.items.length - 1 : state.currentIndex - 1;
-    return {
-      ...state,
-      currentIndex: newIndex,
-    };
-  }
 
-  if (action.type === actionTypes.PHOTOS.SHOW_PREVIOUS) {
-    newIndex = state.currentIndex === state.items.length - 1 ? initialState.currentIndex : state.currentIndex + 1;
-    return {
-      ...state,
-      currentIndex: newIndex,
-    };
+  switch (action.type) {
+    case actionTypes.PHOTOS.SHOW_NEXT:
+      // Loop over to first photo if last photo was reached.
+      newIndex = state.currentIndex === initialState.currentIndex ? state.items.length - 1 : state.currentIndex - 1;
+      return {
+        ...state,
+        currentIndex: newIndex,
+      };
+    case actionTypes.PHOTOS.SHOW_PREVIOUS:
+      // Loop over to last photo if first photo was reached.
+      newIndex = state.currentIndex === state.items.length - 1 ? initialState.currentIndex : state.currentIndex + 1;
+      return {
+        ...state,
+        currentIndex: newIndex,
+      };
+    case actionTypes.PHOTOS.FETCH_ALBUM_SUCCESS:
+      const { album, switchToPhoto } = action.payload;
+      return {
+        ...state,
+        currentIndex: findPhotoIndexForFile(album, switchToPhoto, state.currentIndex),
+        items: album,
+      };
+    default:
+      return state;
   }
-
-  if (action.type === actionTypes.PHOTOS.FETCH_ALBUM_SUCCESS) {
-    const { album, switchToPhoto } = action.payload;
-    return {
-      ...state,
-      currentIndex: findPhotoIndexForFile(album, switchToPhoto, state.currentIndex),
-      items: album,
-    };
-  }
-
-  return state;
 };
 
 export default photosReducer;
