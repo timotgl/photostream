a4a0a309efe57f611545e6b413a8d0d1c27147d0
Timo Taglieber <github@timotaglieber.de>
2019-09-20 13:49:37 +0200

    Destructure state

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -8,9 +8,12 @@ export interface Props {
   total: number;
 }
 
-const mapStateToProps = (state: RootState): Props => ({
-  counter: state.photos.currentIndex + 1,
-  total: state.photos.items.length,
-});
+const mapStateToProps = (state: RootState): Props => {
+  const { currentIndex, items } = state.photos;
+  return {
+    counter: currentIndex + 1,
+    total: items.length,
+  };
+};
 
 export default connect(mapStateToProps)(Counter);
