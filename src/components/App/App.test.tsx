3c3fe06fc1926278844724e47902020f11d84f9a
Timo Taglieber <github@timotaglieber.de>
2019-09-24 14:11:46 +0200

    Prettify app unit test

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -5,7 +5,13 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <App hash="" showNextPhoto={(): null => null} showPreviousPhoto={(): null => null} fetchAlbum={(): null => null} />,
+    <App
+      albumName=""
+      file=""
+      showNextPhoto={(): null => null}
+      showPreviousPhoto={(): null => null}
+      fetchAlbum={(): null => null}
+    />,
     div,
   );
   ReactDOM.unmountComponentAtNode(div);
