7cac68adf546769b9cca758c671987c1e1fabcbf
Timo Taglieber <github@timotaglieber.de>
2022-01-07 13:45:24 +0100

    Remove unused serviceWorker script

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { ConnectedRouter } from 'connected-react-router';
 
 import configureStore, { history } from './redux/configureStore';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 
@@ -19,5 +18,3 @@ render(
   </Provider>,
   document.getElementById('root'),
 );
-
-serviceWorker.unregister();
