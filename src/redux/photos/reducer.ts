c4cf59dd6f1db7df97883a2ac51ee6abb47ca443
Timo Taglieber <github@timotaglieber.de>
2022-01-11 12:33:37 +0100

    Rewrite photos reducer with redux-toolkit's createReducer()

--- src/redux/photos/reducer.ts
+++ src/redux/photos/reducer.ts
@@ -1,6 +1,6 @@
-import actionTypes from '../actionTypes';
-import { AnyAction } from 'redux';
+import { createReducer } from '@reduxjs/toolkit';
 
+import actionTypes from '../actionTypes';
 import { PhotoItem, State } from './interfaces';
 import { fetchAlbum } from './actions';
 
@@ -22,37 +22,27 @@ const findPhotoIndexForFile = (items: Array<PhotoItem>, file: string, currentInd
   return newIndex;
 };
 
-const photosReducer = (state: State = initialState, action: AnyAction): State => {
-  let newIndex: number;
-
-  switch (action.type) {
-    case actionTypes.PHOTOS.SHOW_NEXT:
+const photosReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(actionTypes.PHOTOS.SHOW_NEXT, (state) => {
       // Loop over to first photo if last photo was reached.
-      newIndex = state.currentIndex === initialState.currentIndex ? state.items.length - 1 : state.currentIndex - 1;
-      return {
-        ...state,
-        currentIndex: newIndex,
-      };
-    case actionTypes.PHOTOS.SHOW_PREVIOUS:
+      state.currentIndex =
+        state.currentIndex === initialState.currentIndex ? state.items.length - 1 : state.currentIndex - 1;
+    })
+    .addCase(actionTypes.PHOTOS.SHOW_PREVIOUS, (state) => {
       // Loop over to last photo if first photo was reached.
-      newIndex = state.currentIndex === state.items.length - 1 ? initialState.currentIndex : state.currentIndex + 1;
-      return {
-        ...state,
-        currentIndex: newIndex,
-      };
-    case fetchAlbum.fulfilled.type:
+      state.currentIndex =
+        state.currentIndex === state.items.length - 1 ? initialState.currentIndex : state.currentIndex + 1;
+    })
+    .addCase(fetchAlbum.fulfilled, (state, action) => {
       const { album, albumName, switchToPhoto } = action.payload;
-      return {
-        ...state,
-        currentIndex: switchToPhoto
-          ? findPhotoIndexForFile(album, switchToPhoto, state.currentIndex)
-          : initialState.currentIndex, // Reset currentIndex if there is no photo to switch to via hash url
-        items: album,
-        albumName,
-      };
-    default:
-      return state;
-  }
-};
+      state.currentIndex = switchToPhoto
+        ? findPhotoIndexForFile(album, switchToPhoto, state.currentIndex)
+        : // Reset currentIndex if there is no photo to switch to via hash url;
+          initialState.currentIndex;
+      state.items = album;
+      state.albumName = albumName;
+    });
+});
 
 export default photosReducer;
