6e0bc10de53ae785f0c397ad980acc7c234faaf7
Timo Taglieber <github@timotaglieber.de>
2025-04-28 22:49:04 +0200

    Remove obsolete util functions

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,8 +1,5 @@
 import config from '../config';
 
-export const buildPhotoUrl = (albumName: string, file: string) =>
-  `${config.VITE_PUBLIC_URL}/#${albumName}/${file}`;
-
 // TODO: handle different resolutions
 export const buildPhotoThumbnailUrl = (albumName: string, file: string) =>
   `${config.ALBUM_ROOT}/${albumName}/480/${file}`;
